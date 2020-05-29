bb8925b668f02bc4092f35316871448a865f5500
Steven <tonystevenj@gmail.com>
2020-05-29 12:54:14 -0400

    Finished Jan

--- Mytwint.py
+++ Mytwint.py
@@ -4,9 +4,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from time import strftime, localtime
-import datetime
-
-
+import time
 def getReplyer(name, id):
     url = "https://twitter.com/" + str(name) + "/status/" + str(id)
     # print(url)
@@ -51,8 +49,6 @@ def getReplyer(name, id):
             replies_content.append(replytext)
         # print(replies_to_users)
     return replies_to_users,replies_time,replies_content
-
-
 def get_retweeters_list(tweet_id):
     # get the data of retweets
     r = requests.get('https://twitter.com/i/activity/retweeted_popup?id=' + str(tweet_id))
@@ -64,100 +60,41 @@ def get_retweeters_list(tweet_id):
         'div class=\\\\"account  js-actionable-user js-profile-popup-actionable \\\\" data-screen-name=\\\\"(.+?)\\\\" data-user-id=\\\\"',
         text)
     return x
+def twintScraper(from_date=None, end_date=None):
 
+    # Configure
+    c = twint.Config()
+    # c.Username = "realDonaldTrump"
+    c.Search = "coronavirus"
+    c.Limit = 3200
 
-# Configure
-c = twint.Config()
-# c.Username = "realDonaldTrump"
-c.Search = "coronavirus"
-# c.Limit = 10
-# c.Tweet_id = "1257793742540386304"
-c.Show_hashtags = True
-c.Get_replies = True
-c.Verified = True
-c.Stats = True
-c.Count = True
-c.Lang = "en"
-# print(get_retweeters_list("1258837711806496770"))
-# Run
-# twint.run.Profile(c)
-c.Store_object = True
-c.Since = "2020-01-30 00:00:00"
-c.Until ="2020-02-01 00:00:00"
-# c.Until =str(datetime.datetime.now())[:19]
-# print(str(datetime.datetime.now())[:19])
-# exit()
-twint.run.Search(c)
+    # c.Tweet_id = "1257793742540386304"
+    c.Show_hashtags = True
+    c.Get_replies = True
+    c.Verified = True
+    c.Stats = True
+    c.Count = True
+    c.Lang = "en"
+    c.Hide_output = True
+    # c.Resume = "1223026504482918405"
+    # print(get_retweeters_list("1258837711806496770"))
+    # Run
+    # twint.run.Profile(c)
+    #
+    c.Store_object = True
+    # c.Since = "2020-01-30 00:00:00"
+    # c.Until ="2020-02-01 00:00:00"
+    c.Since = from_date
+    c.Until =end_date
+    # c.Until =str(datetime.datetime.now())[:19]
+    # print(str(datetime.datetime.now())[:19])
+    # exit()
+    twint.run.Search(c)
 
-tweets_as_objects = twint.output.tweets_list
-# print(type(tweets_as_objects))
-# print(tweets_as_objects[0].id)
-dict_op = {
-        "CONTENT": [],
-        "TWEET_ID": [],
-        "USER_NAME": [],
-        "POST_DATE": [],
-        "POST_TIME": [],
-        "LINK": [],
-        "URL_INCLUDED": [],
-        "RETWEETS_COUNT": [],
-        "RETWEETS_PEOPLE": [],
-        "LIKES_AMOUNT": [],
-        "REPLIIES_AMOUNT": [],
-        "REPLAY_PEOPLE": [],
-        "REPLAY_TIME": [],
-        "REPLAY_CONTENT": []
-
-}
-count =0
-for tweet in tweets_as_objects:
-    id = tweet.id
-    name = tweet.username
-    # print(name,"HHHHHHHHHHHHHHHHHHHHHHHH")
-    likes_amount = tweet.likes_count
-    retweets_count = tweet.retweets_count
-    replies_count = tweet.replies_count
-    replies_people=[]
-
-    replies_time=[]
-    replies_content=[]
-    replies_people, replies_time, replies_content=getReplyer(name, id)
-    print(
-        count,
-        " CONTENT: ", tweet.tweet,
-        " TWEET_ID: ", str(id),
-        " USER_NAME: ", str(name),
-        " POST_DATE: ",tweet.datestamp,
-        " POST_TIME: ",tweet.timestamp,
-        " LINK: ", tweet.link,
-        " URL_INCLUDED: ", tweet.urls,
-        " RETWEETS_COUNT: ", len(get_retweeters_list(id)),
-        " RETWEETS_PEOPLE: ", get_retweeters_list(id),
-        " LIKES_AMOUNT: ", likes_amount,
-        " REPLIIES_AMOUNT: ", len(replies_people),
-        " REPLAY_PEOPLE: ", replies_people,
-        " REPLAY_TIME: ", replies_time,
-        " REPLAY_CONTENT: ", replies_content
-    )
-    dict_op["CONTENT"].append(tweet.tweet)
-    dict_op["TWEET_ID"].append(str(id))
-    dict_op["USER_NAME"].append(str(name))
-    dict_op["POST_DATE"].append(tweet.datestamp)
-    dict_op["POST_TIME"].append(tweet.timestamp)
-    dict_op["LINK"].append(tweet.link)
-    dict_op["URL_INCLUDED"].append(tweet.urls)
-    dict_op["RETWEETS_COUNT"].append(len(get_retweeters_list(id)))
-    dict_op["RETWEETS_PEOPLE"].append(get_retweeters_list(id))
-    dict_op["LIKES_AMOUNT"].append(likes_amount)
-    dict_op["REPLIIES_AMOUNT"].append(len(replies_people))
-    dict_op["REPLAY_PEOPLE"].append(replies_people)
-    dict_op["REPLAY_TIME"].append(replies_time)
-    dict_op["REPLAY_CONTENT"].append(replies_content)
-    count+=1
-    if count%100==0 and count!=0:
-        df = pd.DataFrame(data=dict_op)
-        df.to_json(f"{count} COVID-19.json",orient='records')
-        dict_op = {
+    tweets_as_objects = twint.output.tweets_list
+    # print(type(tweets_as_objects))
+    # print(tweets_as_objects[0].id)
+    dict_op = {
             "CONTENT": [],
             "TWEET_ID": [],
             "USER_NAME": [],
@@ -172,4 +109,90 @@ for tweet in tweets_as_objects:
             "REPLAY_PEOPLE": [],
             "REPLAY_TIME": [],
             "REPLAY_CONTENT": []
-        }
+
+    }
+    count =0
+    for tweet in tweets_as_objects:
+        id = tweet.id
+        name = tweet.username
+        # print(name,"HHHHHHHHHHHHHHHHHHHHHHHH")
+        likes_amount = tweet.likes_count
+        retweets_count = tweet.retweets_count
+        replies_count = tweet.replies_count
+        replies_people=[]
+
+        replies_time=[]
+        replies_content=[]
+        replies_people, replies_time, replies_content=getReplyer(name, id)
+        # print(
+        #     count,
+        #     " CONTENT: ", tweet.tweet,
+        #     " TWEET_ID: ", str(id),
+        #     " USER_NAME: ", str(name),
+        #     " POST_DATE: ",tweet.datestamp,
+        #     " POST_TIME: ",tweet.timestamp,
+        #     " LINK: ", tweet.link,
+        #     " URL_INCLUDED: ", tweet.urls,
+        #     " RETWEETS_COUNT: ", len(get_retweeters_list(id)),
+        #     " RETWEETS_PEOPLE: ", get_retweeters_list(id),
+        #     " LIKES_AMOUNT: ", likes_amount,
+        #     " REPLIIES_AMOUNT: ", len(replies_people),
+        #     " REPLAY_PEOPLE: ", replies_people,
+        #     " REPLAY_TIME: ", replies_time,
+        #     " REPLAY_CONTENT: ", replies_content
+        # )
+        dict_op["CONTENT"].append(tweet.tweet)
+        dict_op["TWEET_ID"].append(str(id))
+        dict_op["USER_NAME"].append(str(name))
+        dict_op["POST_DATE"].append(tweet.datestamp)
+        dict_op["POST_TIME"].append(tweet.timestamp)
+        dict_op["LINK"].append(tweet.link)
+        dict_op["URL_INCLUDED"].append(tweet.urls)
+        dict_op["RETWEETS_COUNT"].append(len(get_retweeters_list(id)))
+        dict_op["RETWEETS_PEOPLE"].append(get_retweeters_list(id))
+        dict_op["LIKES_AMOUNT"].append(likes_amount)
+        dict_op["REPLIIES_AMOUNT"].append(len(replies_people))
+        dict_op["REPLAY_PEOPLE"].append(replies_people)
+        dict_op["REPLAY_TIME"].append(replies_time)
+        dict_op["REPLAY_CONTENT"].append(replies_content)
+        count+=1
+        if count%200==0 and count!=0:
+            lastsavedtweetid = dict_op["TWEET_ID"][199]
+            print(f"SAVE_MARK {count}: lasts aved tweetid = ",lastsavedtweetid)
+            df = pd.DataFrame(data=dict_op)
+            df.to_json(f"{from_date} {count} COVID-19.json",orient='records')
+            dict_op = {
+                "CONTENT": [],
+                "TWEET_ID": [],
+                "USER_NAME": [],
+                "POST_DATE": [],
+                "POST_TIME": [],
+                "LINK": [],
+                "URL_INCLUDED": [],
+                "RETWEETS_COUNT": [],
+                "RETWEETS_PEOPLE": [],
+                "LIKES_AMOUNT": [],
+                "REPLIIES_AMOUNT": [],
+                "REPLAY_PEOPLE": [],
+                "REPLAY_TIME": [],
+                "REPLAY_CONTENT": []
+            }
+        if count%1000==0 and count!=0:
+            time.sleep(60.0)
+# c.Since = "2020-01-30 00:00:00"
+# c.Until ="2020-02-01 00:00:00"
+
+# twintScraper(from_date="2020-01-31" ,end_date="2020-02-01" )
+# twintScraper(from_date="2020-01-30" ,end_date="2020-01-31" )
+# twintScraper(from_date="2020-01-29" ,end_date="2020-01-30" )
+# twintScraper(from_date="2020-01-28" ,end_date="2020-01-29" )
+# twintScraper(from_date="2020-01-27" ,end_date="2020-01-28" )
+
+for i in reversed(range(10,24)):
+    from_date = f"2020-01-{i}"
+    end_date = f"2020-01-{i+1}"
+    print(f"DAY_MARK: ",from_date," ",end_date)
+    try:
+        twintScraper(from_date=from_date ,end_date=end_date )
+    except:
+        pass
