b1509062838a0bbd8f41300638e347d004311424
Steven <tonystevenj@gmail.com>
2020-06-12 15:58:23 -0400

    fixed: retweets doesn't work

--- Mytwint.py
+++ Mytwint.py
@@ -55,7 +55,7 @@ def getReplyer(name, id):
     return replies_to_users,replies_time,replies_content
 def get_retweeters_list(tweet_id):
     # get the data of retweets
-    r = requests.get('https://twitter.com/i/activity/retweeted_popup?id=' + str(tweet_id))
+    r = requests.get('https://twitter.com/i/activity/retweeted_popup?id=' + str(tweet_id), headers={"X-Requested-With": "XMLHttpRequest"})
     # r = requests.get('https://twitter.com/BryanAlexander/status/' + str(tweet_id))
     # use the grep in order to get the retweeters
 
@@ -68,9 +68,9 @@ def twintScraper(from_date=None, end_date=None):
 
     # Configure
     c = twint.Config()
-    # c.Username = "realDonaldTrump"
-    c.Search = "coronavirus"
-    c.Limit = 4700
+    c.Username = "realDonaldTrump"
+    # c.Search = "coronavirus"
+    c.Limit = 10
 
     # c.Tweet_id = "1257793742540386304"
     c.Show_hashtags = True
@@ -125,23 +125,23 @@ def twintScraper(from_date=None, end_date=None):
         replies_time=[]
         replies_content=[]
         replies_people, replies_time, replies_content=getReplyer(name, id)
-        # print(
-        #     count,
-        #     " CONTENT: ", tweet.tweet,
-        #     " TWEET_ID: ", str(id),
-        #     " USER_NAME: ", str(name),
-        #     " POST_DATE: ",tweet.datestamp,
-        #     " POST_TIME: ",tweet.timestamp,
-        #     " LINK: ", tweet.link,
-        #     " URL_INCLUDED: ", tweet.urls,
-        #     " RETWEETS_COUNT: ", len(get_retweeters_list(id)),
-        #     " RETWEETS_PEOPLE: ", get_retweeters_list(id),
-        #     " LIKES_AMOUNT: ", likes_amount,
-        #     " REPLIIES_AMOUNT: ", len(replies_people),
-        #     " REPLAY_PEOPLE: ", replies_people,
-        #     " REPLAY_TIME: ", replies_time,
-        #     " REPLAY_CONTENT: ", replies_content
-        # )
+        print(
+            count,
+            " CONTENT: ", tweet.tweet,
+            " TWEET_ID: ", str(id),
+            " USER_NAME: ", str(name),
+            " POST_DATE: ",tweet.datestamp,
+            " POST_TIME: ",tweet.timestamp,
+            " LINK: ", tweet.link,
+            " URL_INCLUDED: ", tweet.urls,
+            " RETWEETS_COUNT: ", len(get_retweeters_list(id)),
+            " RETWEETS_PEOPLE: ", get_retweeters_list(id),
+            " LIKES_AMOUNT: ", likes_amount,
+            " REPLIIES_AMOUNT: ", len(replies_people),
+            " REPLAY_PEOPLE: ", replies_people,
+            " REPLAY_TIME: ", replies_time,
+            " REPLAY_CONTENT: ", replies_content
+        )
         dict_op["CONTENT"].append(tweet.tweet)
         dict_op["TWEET_ID"].append(str(id))
         dict_op["USER_NAME"].append(str(name))
@@ -193,7 +193,8 @@ def twintScraper(from_date=None, end_date=None):
 
 
 
-twintScraper(from_date="2020-02-29" ,end_date="2020-03-01" )
+# twintScraper(from_date="2020-02-29" ,end_date="2020-03-01" )
+twintScraper(from_date="2020-02-28" ,end_date="2020-02-29" )
 # for i in reversed(range(10,14)):
 #     from_date = f"2020-02-{i}"
 #     end_date = f"2020-02-{i+1}"
